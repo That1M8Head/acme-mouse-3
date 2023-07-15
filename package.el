691e8121c8067a59b8c2832c295b5324fc79d708
Arsalan Kazmi <sonicspeed848@gmail.com>
2023-07-15 22:03:23 +0100

    Create package.el

---
+++ package.el
@@ -0,0 +1,19 @@
+;;; package.el --- acme-mouse-3 package metadata
+
+;;; Commentary:
+
+;; This package lets you evaluate (run) Elisp code using the right mouse button, inspired by the "Execute text" command in Rob Pike’s Acme editor. 
+
+;;; Code:
+
+(define-package "acme-mouse-3" "1.0"
+  "Evaluate Elisp code using the right mouse button (mouse-3)."
+  '((emacs "24.4"))
+  :keywords '("convenience" "evaluation")
+  :url "https://github.com/That1M8Head/acme-mouse-3"
+  :author "Arsalan Kazmi"
+  :maintainer "sonicspeed848@gmail.com")
+
+(provide 'acme-mouse-3)
+
+;;; package.el ends here
